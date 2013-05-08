1c20967d112d5d299155016c092c2ee2ad4fc2bc
91marek@gmail.com <91marek@gmail.com@7194e1f5-fb86-ad18-7df1-171303e3658d>
2013-05-08 21:34:53 +0000

    Passing relations as a start-up argument to DA.

--- src/DA.java
+++ src/DA.java
@@ -1,9 +1,27 @@
+import java.util.ArrayList;
+
 import jade.core.Agent;
 import jade.core.behaviours.TickerBehaviour;
 
 @SuppressWarnings("serial")
 public class DA extends Agent {
+	private ArrayList<String> relations = new ArrayList<String>();
+
 	protected void setup() {
+		System.out.println("DIPRE Agent " + getAID().getName() + " is ready.");
+
+		// Get the relations as a start-up argument
+		Object[] args = getArguments();
+		if (args != null) {
+			for (int i = 0; i < args.length; i++) {
+				relations.add(args[i].toString());
+			}
+		}
+		System.out.println("Relations of agent " + getLocalName() + ":");
+		for (String s : relations) {
+			System.out.println(s);
+		}
+
 		addBehaviour(new TickerBehaviour(this, 3000) {
 			protected void onTick() {
 				System.out.println("This is DIPRE Agent! My name is "
