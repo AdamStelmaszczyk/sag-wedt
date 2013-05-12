88aa71d4ea0d47d02b056dd4001c95af655160e3
StelmaszczykAdam@gmail.com <StelmaszczykAdam@gmail.com@7194e1f5-fb86-ad18-7df1-171303e3658d>
2013-05-12 20:06:38 +0000

    Added gson to classpath in run.sh

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 # Windows ;
 # Unix :
 # Semicolon between agents must be escaped using \
-CP=lib/jade.jar:bin
+CP=lib/jade.jar:lib/gson-2.2.3.jar:bin
 java -cp $CP jade.Boot -gui -local-host 127.0.0.1 -local-port 1099 &
 sleep 1
 java -cp $CP jade.Boot -container -local-host 127.0.0.1 -host localhost -port 1099 \
