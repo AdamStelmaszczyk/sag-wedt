a74b50965d17f0c1dcdd7aa495ed4542a8f7eba3
StelmaszczykAdam@gmail.com <StelmaszczykAdam@gmail.com@7194e1f5-fb86-ad18-7df1-171303e3658d>
2013-05-12 12:04:20 +0000

    Setting explicitly -local-host 127.0.0.1 in run.sh to resolve "No ICP active" problem

--- run.sh
+++ run.sh
@@ -6,8 +6,8 @@
 CP=lib/jade.jar:bin
 java -cp $CP jade.Boot -gui -local-host 127.0.0.1 -local-port 1099 &
 sleep 1
-java -cp $CP jade.Boot -container -host localhost -port 1099 \
+java -cp $CP jade.Boot -container -local-host 127.0.0.1 -host localhost -port 1099 \
 -agents bing:BingSA\;crawler:DA\(Isaac\ Asimow\#The\ Robots\ of\ Dawn,David\ Brin\#Startide\ Rising\) &
 sleep 1
-java -cp $CP jade.Boot -container -host localhost -port 1099 \
+java -cp $CP jade.Boot -container -local-host 127.0.0.1 -host localhost -port 1099 \
 -agents bing2:BingSA\;crawler2:DA\(Charles\ Dickens\#Great\ Expectations,William\ Shakespeare\#The\ Comedy\ of\ Errors\)
