a6bd5830b0b16965144b774e22cdf748e6f1c907
piotrowski.przemek@gmail.com <piotrowski.przemek@gmail.com@7194e1f5-fb86-ad18-7df1-171303e3658d>
2013-05-12 15:16:02 +0000

    Zamiana ArrayList na List w deklaracji. Tez przeciwiczenie przeze mnie commitowania w tym środowisku...

--- src/Links.java
+++ src/Links.java
@@ -1,9 +1,10 @@
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.List;
 
 public class Links implements Serializable {
 
-	private final ArrayList<String> links = new ArrayList<String>();
+	private final List<String> links = new ArrayList<String>();
 	private static final long serialVersionUID = 1L;
 
 	public void add(String link) {
