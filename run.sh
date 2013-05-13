c814e3ad8bf98d73c24553f16a4af3d521bc2186
StelmaszczykAdam@gmail.com <StelmaszczykAdam@gmail.com@7194e1f5-fb86-ad18-7df1-171303e3658d>
2013-05-13 17:41:52 +0000

    Running one BingSA and one FarooSA

--- run.sh
+++ run.sh
@@ -10,4 +10,4 @@ java -cp $CP jade.Boot -container -local-host 127.0.0.1 -host localhost -port 10
 -agents faroo:search.FarooSA\;viewer:view.VA\;crawler:dipre.DA\(Isaac\ Asimow\#The\ Robots\ of\ Dawn,David\ Brin\#Startide\ Rising\) &
 sleep 1
 java -cp $CP jade.Boot -container -local-host 127.0.0.1 -host localhost -port 1099 \
--agents faroo2:search.FarooSA\;crawler2:dipre.DA\(Charles\ Dickens\#Great\ Expectations,William\ Shakespeare\#The\ Comedy\ of\ Errors\)
+-agents bing:search.BingSA\;crawler2:dipre.DA\(Charles\ Dickens\#Great\ Expectations,William\ Shakespeare\#The\ Comedy\ of\ Errors\)
